75e66eb7aec476679ef8cfc2a0b6a89b57099707
meswain1123 <meswain@gmail.com>
2020-06-22 16:21:29 -0600

    Testing some things

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff57cc9a0c6aedc368e5b965343619b2.js"
+  "/precache-manifest.5642416aac4774496fefe5f9c643d403.js"
 );
 
 self.addEventListener('message', (event) => {
